091b5fd67ac9b1e12f0590300f155675a0e372e2
mikasa654 <108337991+mikasa654@users.noreply.github.com>
2024-04-10 12:21:38 -0300

    Add files via upload

---
+++ exemplos/main.js
@@ -0,0 +1,9 @@
+const dias_falta = document.querySelectorA11(".dias_restantes");
+
+function calcula (){
+    consttempoObjetivo1= new Date(2024-04-30T00:00:00);
+ //    const tempoObijetivo1 = new Date("2024-04-30T23:59;59");
+  
+  let tempoAtual=new Date ();
+
+}
